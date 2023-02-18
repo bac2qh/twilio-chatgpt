e3f4b05445c1beaf2fe2b9281d0a8dc156990c72
Xin Ding <xinding@Xins-MacBook-Pro.local>
2023-02-18 16:47:00 -0600

    image baby

--- flaskr/main.py
+++ flaskr/main.py
@@ -13,7 +13,7 @@ bp = Blueprint('main', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, img, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -26,6 +26,7 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        img = request.form['img']
         error = None
 
         if not title:
@@ -35,9 +36,9 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                'VALUES (?,?,?)',
-                (title, body, g.user['id'])
+                'INSERT INTO post (title, body, img, author_id)'
+                'VALUES (?,?,?,?)',
+                (title, body, img, g.user['id'])
             )
             db.commit()
             return redirect(url_for('main.index'))
@@ -47,7 +48,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, img, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id =?',
         (id, )
@@ -70,6 +71,7 @@ def update(id):
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
+        img = request.form['img']
         error = None
 
         if not title:
@@ -80,9 +82,9 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title =?, body =?'
+                'UPDATE post SET title =?, body =?, img=?'
                 'WHERE id =?',
-                (title, body, id)
+                (title, body, img, id)
             )
             db.commit()
             return redirect(url_for('main.index'))
